a11a1a77f27a8f587a8332116cf60fb9e6599c91
dfdecurt11-droid <dfdecurt.11@gmail.com>
2026-05-26 11:29:49 -0500

    Corregir conexión de base de datos usando variable de entorno

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,9 @@
+// backend/db.js
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres", // tu usuario
-  host: "localhost", // servidor DB
-  database: "Taller_Sandra", // nombre de tu BD
-  password: "database", // tu contraseña
-  port: 5432, // puerto por defecto
+  connectionString: process.env.DATABASE_URL, // Render te da esta variable
+  ssl: { rejectUnauthorized: false } // Render requiere SSL
 });
 
 module.exports = pool;
