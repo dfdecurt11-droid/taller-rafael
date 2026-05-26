215daaf06a6fcd0feb9318813be7e2fe20b619ae
dfdecurt11-droid <dfdecurt.11@gmail.com>
2026-05-26 17:21:56 -0500

    cambios

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,16 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
+<<<<<<< Updated upstream
   connectionString: process.env.DATABASE_URL, // Render te da esta variable
   ssl: { rejectUnauthorized: false } // Render requiere SSL
+=======
+  user: "postgres", // tu usuario
+  host: "localhost", // servidor DB
+  database: "Taller_Sandra", // nombre de tu BD
+  password: "database", // tu contraseña
+  port: 5432, // puerto por defecto
+>>>>>>> Stashed changes
 });
 
 module.exports = pool;
